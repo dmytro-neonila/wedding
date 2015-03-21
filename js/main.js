8d096f6e4bf3da1eef77de4fe3e7d322bb99eefd
Dmytro Yarmak <dmytroyarmak@gmail.com>
2015-03-21 11:44:31 +0200

    Fix problem with initial scrooll position and change flowers

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 $(document).ready( function() {
-	$(window).on("scroll", _.debounce(function(){
-		var screen = $(window).height(),
-			scrollPos = $(this).scrollTop(),
-			diff = screen - scrollPos;
+	$(window)
+		.scrollTop(0)
+		.on("scroll", _.debounce(function(){
+			var screen = $(window).height(),
+				scrollPos = $(this).scrollTop(),
+				diff = screen - scrollPos;
 
-		if (diff > 0 && diff < screen*0.75) {
-            $(".bg-chevron").velocity("scroll", { duration: 400, easing: "ease-out" });
-        }
-	}, 500));
+			if (diff > 0 && diff < screen*0.75) {
+	            $(".bg-chevron").velocity("scroll", { duration: 400, easing: "ease-out" });
+	        }
+		}, 500));
 });
