406e96e24a580a56037d4a1fd032e4c29fabc07e
nilaneo <shlikhtenkoneonila@gmail.com>
2015-03-24 18:49:28 +0200

    Add jcarousel

--- js/main.js
+++ js/main.js
@@ -4,6 +4,36 @@ $(document).ready( function() {
 	detectDevice();
 	drawMap();
 
+	// Jcarousel
+
+	$('.jcarousel')
+        .jcarousel({
+            wrap: 'circular'
+        });
+
+    $('.jcarousel-control-prev')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '-=1'
+            });
+
+        $('.jcarousel-control-next')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '+=1'
+            });
+
+
 	function initAutoScroll () {
 		$(window)
 			.scrollTop(0)
